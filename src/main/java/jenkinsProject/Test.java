05934790fd3d7568337f8998466b5e3688e19c25
simranrathore <simran.rathore@nagarro.com>
2023-01-16 14:21:14 +0530

    jenkins git integration check

---
+++ src/main/java/jenkinsProject/Test.java
@@ -0,0 +1,11 @@
+package jenkinsProject;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello from jenkins");
+
+	}
+
+}
